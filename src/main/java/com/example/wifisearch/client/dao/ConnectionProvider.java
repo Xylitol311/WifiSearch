f8f203522e0de6c2e90dc5035261fba0c45e4a7e
Xylitol311 <cktnghd0311@daum.net>
2023-10-05 22:51:26 +0900

    Feat: Connection Provider 생성

---
+++ src/main/java/com/example/wifisearch/client/dao/ConnectionProvider.java
@@ -0,0 +1,33 @@
+package com.example.wifisearch.client.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private static Connection connection;
+    private static final String dbUrl = "jdbc:sqlite:test.db";
+    
+    private ConnectionProvider() {
+    }
+    
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+        try {
+            connection = DriverManager.getConnection(dbUrl);
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException("", e);
+        }
+    }
+    
+    public static void close(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
